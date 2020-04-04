c9c6be51382b0bbbae03f1b7cf3e718ff56a5bc9
chess-equality <chessequality@gmail.com>
2020-04-04 21:03:31 +0800

    Fixed microk8s registry cleanup

--- microk8s.sh
+++ microk8s.sh
@@ -8,7 +8,10 @@ docker rmi --force "$(docker inspect --format="{{.Id}}" localhost:32000/dapr-go-
 docker build -f adder/Dockerfile adder/. -t localhost:32000/dapr-go-calculator-adder:registry
 docker build -f react-calculator/Dockerfile react-calculator/. -t localhost:32000/dapr-go-calculator-react-frontend:registry
 
-microk8s.ctr images rm --sync "$(microk8s.ctr images list -q | grep dapr-go-calculator)"
+microk8s.ctr images rm localhost:32000/dapr-go-calculator-adder:registry
+microk8s.ctr images rm localhost:32000/dapr-go-calculator-react-frontend:registry
+microk8s.ctr images rm --sync "$(microk8s.ctr images list -q | grep localhost:32000/dapr-go-calculator-adder@sha256)"
+microk8s.ctr images rm --sync "$(microk8s.ctr images list -q | grep localhost:32000/dapr-go-calculator-react-frontend@sha256)"
 
 docker push localhost:32000/dapr-go-calculator-adder:registry
 docker push localhost:32000/dapr-go-calculator-react-frontend:registry
